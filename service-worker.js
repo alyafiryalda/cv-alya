f434923d2c717adb18aa4fa33f01d6216d4781b5
alyafiryalda <149244268+alyafiryalda@users.noreply.github.com>
2024-11-07 12:53:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+
 const CACHE_NAME = "Alya";
 const urlsToCache = [
   "/",
@@ -55,4 +64,35 @@ self.addEventListener("fetch", (event) => {
       return response || fetch(event.request);
     })
   );
+});
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyB5cojTv5NMcVrH6Or8daHs17YhzBFIGz8",
+    authDomain: "cv-alya.firebaseapp.com",
+    projectId: "cv-alya",
+    storageBucket: "cv-alya.firebasestorage.app",
+    messagingSenderId: "1045703852496",
+    appId: "1:1045703852496:web:ac3e331c782cd61959b9d5",
+    measurementId: "G-S9LX0GFJ9S"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Website Portofolio";
+  const notificationOptions = {
+    body: "Notifikasi cv alya telah muncul.",
+    icon: "/images/kucing.jpg",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
